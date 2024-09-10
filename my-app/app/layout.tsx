c8a89a8bde153cd42b594dac7d60ec48d9059289
Shota Tonari <tona.shota@gmail.com>
2024-09-10 15:15:25 -0700

    Commit

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <header className="px-4 lg:px-6 h-14 flex items-center justify-center">
             <div className="container flex items-center justify-between">
               <a className="flex items-center justify-center" href="/">
-                <span className="font-bold">Shota Tonari</span>
+                <span className="font-bold">Home</span>
               </a>
               <nav className="flex gap-4 sm:gap-6">
                 <a className="text-sm font-medium hover:underline underline-offset-4" href="/education">
